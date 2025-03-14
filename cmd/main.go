f780d85c2a5b3abe9d7b132b4423a345c11b3f6a
admcurlyvld <gzdec337@mail.ru>
2025-03-14 18:32:53 +0400

    Add and connect db

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,43 @@ import (
 	"ToDoApp/pkg/handler"
 	"ToDoApp/pkg/repository"
 	"ToDoApp/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("error initializing db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(ToDoApp.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error starting server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("configs")
+	return viper.ReadInConfig()
 }
