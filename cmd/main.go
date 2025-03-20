fe596784488b49333f5f351aa165b8d6884712f7
admcurlyvld <gzdec337@mail.ru>
2025-03-20 20:54:47 +0400

    Add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,10 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"golang.org/x/net/context"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -36,8 +39,23 @@ func main() {
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(ToDoApp.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("error starting server: %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error starting server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	logrus.Print("TodoApp Shutting Down")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error shutting down server: %s", err.Error())
+	}
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error closing db: %s", err.Error())
 	}
 }
 
